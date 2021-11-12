3c7faf3d91655dbfa7b880db947090eaefbb7408
Richard Anabeto Opoku <anabeto93@gmail.com>
2021-11-12 16:28:08 +0000

    now using mysql.

--- bookstore_users-api/datasources/mysql/users_db/create_users.sql
+++ bookstore_users-api/datasources/mysql/users_db/create_users.sql
@@ -1,8 +1,9 @@
 create TABLE IF NOT EXISTS `users` (
-    id BIGINT(20) AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name varchar(100) NOT NULL,
     last_name varchar(100) NOT NULL,
-    email varchar(100) NOT NULL,
+    email varchar(100) NOT NULL UNIQUE,
     date_created DATETIME NOT NULL,
-    date_updated DATETIME DEFAULT now()
+    date_updated DATETIME DEFAULT now(),
+    UNIQUE INDEX email_UNIQUE (`email` ASC)
 );
--- bookstore_users-api/domain/users/user_dao.go
+++ bookstore_users-api/domain/users/user_dao.go
@@ -1,67 +1,94 @@
 package users
 
 import (
+	"database/sql"
 	"fmt"
+	"strings"
 
 	"github.com/anabeto93/bookstore/bookstore_users-api/datasources/mysql/users_db"
 	"github.com/anabeto93/bookstore/bookstore_users-api/utils/date_utils"
 	"github.com/anabeto93/bookstore/bookstore_users-api/utils/errors"
 )
 
-var(
-	usersDB = make(map[int64]*User)
-)
-
 const (
 	insertQuery = "INSERT INTO users (email, first_name, last_name, date_created) VALUES (?, ?, ?, ?)"
+	getUserQuery = "SELECT id, first_name, last_name, email, date_created FROM users WHERE id = ? LIMIT 1;"
+	getUserByEmailQuery = "SELECT id, first_name, last_name, email, date_created FROM users WHERE email = ? LIMIT 1;"
+
 )
 
 func (user User) Find(userId int64) (*User, *errors.RestErr) {
-	if err := users_db.Client.Ping(); err != nil {
-		panic(err)
+	var result User
+
+	stmnt, err := prepare(getUserQuery); if  err != nil {
+		return nil, err
 	}
-	result := usersDB[userId]
-	if result == nil {
+
+	defer stmnt.Close()
+
+	sqlUser := stmnt.QueryRow(userId)
+	fmt.Println(sqlUser)
+	if sqlErr := sqlUser.Scan(&result.Id, &result.Email, &result.FirstName, &result.LastName, &result.DateCreated); err != nil {
+		return nil, errors.NewInternalServerError(fmt.Sprintf("Error fetching user: %s", sqlErr.Error()))
+	}
+
+	if (result == User{}) {
 		return nil, errors.NewNotFoundError(fmt.Sprintf("user %d not found", userId))
 	}
-	return result, nil
+
+	return &result, nil
 }
 
 func (user User) FindByEmail(email string) (*User, *errors.RestErr) {
-	var result *User
+	var result User
 
-	for _, v := range usersDB {
-		if v.Email == email {
-			result = v
-		} else {
-			result = nil
-		}
+	stmnt, err := prepare(getUserByEmailQuery); if  err != nil {
+		return nil, err
 	}
 
-	if result != nil {
-		return result, nil
+	defer stmnt.Close()
+
+	sqlUser := stmnt.QueryRow(email)
+	if sqlErr := sqlUser.Scan(&result.Id, &result.Email, &result.FirstName, &result.LastName, &result.DateCreated); err != nil {
+		return nil, errors.NewInternalServerError(fmt.Sprintf("Error fetching user: %s", sqlErr.Error()))
+	}
+
+	if (result != User{}) {
+		return &result, nil
 	}
 
 	return nil, errors.NewNotFoundError(fmt.Sprintf("user with email %s not found", email))
 }
 
 func (user *User) Save() *errors.RestErr {
-	stmnt, err := users_db.Client.Prepare(insertQuery); if err != nil {
-		return errors.NewInternalServerError(fmt.Sprintf("Could not prepare sql insert statement: %s", err.Error()))
+	stmnt, err := prepare(insertQuery); if err != nil {
+		return err
 	}
+
 	defer stmnt.Close()
 
 	user.DateCreated = date_utils.GetNowString("2006-01-02 15:04:05")
-	result, err := stmnt.Exec(user.Email, user.FirstName, user.LastName, user.DateCreated)
+	result, sqlErr := stmnt.Exec(user.Email, user.FirstName, user.LastName, user.DateCreated)
 
-	if err != nil {
-		return errors.NewInternalServerError(fmt.Sprintf("Error saving user: %s", err.Error()))
+	if sqlErr != nil {
+		if strings.Contains(sqlErr.Error(), "Duplicate entry") {
+			return errors.NewBadRequestError(fmt.Sprintf("email %s already exists", user.Email))			
+		}
+		return errors.NewInternalServerError(fmt.Sprintf("Error saving user: %s", sqlErr.Error()))
 	}
 
-	userId, err := result.LastInsertId(); if err != nil {
-		return errors.NewInternalServerError(fmt.Sprintf("Error saving user: %s", err.Error()))
+	userId, sqlErr := result.LastInsertId(); if sqlErr != nil {
+		return errors.NewInternalServerError(fmt.Sprintf("Error saving user: %s", sqlErr.Error()))
 	}
 	
 	user.Id = userId
 	return nil
+}
+
+func prepare(query string) (*sql.Stmt, *errors.RestErr) {
+	stmnt, err := users_db.Client.Prepare(query); if err != nil {
+		return nil, errors.NewInternalServerError(fmt.Sprintf("Could not prepare sql statement: %s", err.Error()))
+	}
+
+	return stmnt, nil
 }
